9be6f56bf9f4538eb5a9c82f9b72ada25a217a86
HyeonJu LEE <wkdal3512@dankook.ac.kr>
2024-07-05 08:07:52 +0900

    🔥 remove: Phone을 사고, 켜는 행위를 Person의 행위로 간주하고 제거

--- src/buy_phone/Phone.java
+++ src/buy_phone/Phone.java
@@ -9,14 +9,5 @@ public abstract class Phone {
 
     public Phone() {}
 
-    void buyPhone() {
-        System.out.println(user + "님이 " + brand + "폰을 구매했습니다.");
-    }
-
-    void turnOn() {
-        System.out.println(user + "님이 " + brand + "폰을 켰습니다.");
-        loading();
-    }
-
     abstract void loading();
 }
